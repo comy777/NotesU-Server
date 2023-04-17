bf937b7e1c87ff6e4b33410ed10da891608245cb
sebastian <carrillosebastian1@gmail.com>
2023-04-17 08:46:52 -0500

    Fix added gmail variables

--- src/nodemailer/email.ts
+++ src/nodemailer/email.ts
@@ -6,8 +6,8 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
-    user: 'servercomy777@gmail.com',
-    pass: 'hcwvfktbuhnjqsmb'
+    user: process.env.GMAIL_ACCOUNT,
+    pass: process.env.GMAIL_PASSWORD
   }
 });
 
