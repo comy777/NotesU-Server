8b233ef1ea844edd3e8d2310c45f049f1076aaa7
sebastian <carrillosebastian1@gmail.com>
2023-04-25 13:09:49 -0500

    Fix validate category

--- src/utils/categories.ts
+++ src/utils/categories.ts
@@ -2,7 +2,7 @@ import Categories from "../models/Categories"
 
 export const validateCategorie = async (categorie: string, user: string, id?: string) => {
   try {
-    const query = { categorie, user }
+    const query = { categorie: categorie.toLowerCase(), user }
     return id ? await Categories.findById(id) : await Categories.findOne(query)
   } catch (error) {
     console.log(error)
